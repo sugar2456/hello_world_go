fc2b0ad421b7f9d6286cdaecc7c3b911cadfa296
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-04-29 04:49:07 +0000

    gin導入とalbum apiを実装

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"hello_world_go/pkg/greetings"
-	"hello_world_go/pkg/hello"
-	"log"
+	"hello_world_go/pkg/albums"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	message := hello.SayHello()
-	fmt.Println(message)
+	router := gin.Default()
+	router.GET("/albums", albums.GetAlbums)
 
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	names := []string{"Gladys", "Samantha", "Darrin"}
-	greeting, err := greetings.His(names)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(greeting)
+	router.Run("localhost:8080")
 }
---
+++ pkg/albums/getAlbums.go
@@ -0,0 +1,18 @@
+package albums
+
+import (
+	"hello_world_go/pkg/types"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var albums = []types.Album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func GetAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
