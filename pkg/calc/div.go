a2a9a0a112e20fa9d7a6a8461cd6505ad8cc05a9
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-09-15 22:30:16 +0900

    名前付き戻り値を実装

--- pkg/calc/div.go
+++ pkg/calc/div.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 )
 
-func divAndRemainder(num, denom int) (int, int, error) {
+func divAndRemainder(num, denom int) (result int, remainder int, err error) {
 	if denom == 0 {
 		return 0, 0, errors.New("0で割ることはできません")
 	}
-	return num / denom, num % denom, nil
+	result = num / denom
+	remainder = num % denom
+	return result, remainder, nil
 }
