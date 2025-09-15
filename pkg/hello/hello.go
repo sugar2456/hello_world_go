850d0570def80a607e719e93ea691a339d8c4bc2
sugar2456 <sugar2456_0526@yahoo.co.jp>
2025-09-15 18:03:42 +0900

    hello関数更新

--- pkg/hello/hello.go
+++ pkg/hello/hello.go
@@ -1,5 +1,7 @@
 package hello
 
-func SayHello() string {
-    return "Hello, World!"
+import "fmt"
+
+func SayHello(param string) string {
+	return fmt.Sprintf("Hello, World! %s", param)
 }
--- pkg/hello/hello_test.go
+++ pkg/hello/hello_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestSayHello(t *testing.T) {
-	expected := "Hello, World!"
-	msg := SayHello()
+	expected := "Hello, World! Have a great day!"
+	msg := SayHello("Have a great day!")
 	if msg != expected {
 		t.Errorf(`SayHello() = %q, want %q`, msg, expected)
 	}
